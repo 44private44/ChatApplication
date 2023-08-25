af40aa936204bdd4570a49cebaf06f4b84a703f8
44private44 <goodjob3092001@gmail.com>
2023-08-25 14:36:09 +0530

    Update the css and js code

--- js/client.js
+++ js/client.js
@@ -7,7 +7,8 @@ var audio = new Audio('../images/ting.mp3');
 
 const append = (message, position) => {
     const messageElement = document.createElement('div');
-    messageElement.innerText = message;
+    // messageElement.innerText = message;
+    messageElement.innerHTML = message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
@@ -16,26 +17,39 @@ const append = (message, position) => {
     }
 }
 
+// ask a new user name for the add on chat and send to the server
 const name = prompt("Enter your name to join");
 socket.emit('new-user-joined', name);
 
+// new user join receive the event from the server 
 socket.on('user-joined', data => {
-    append(` ${data} joined the chat`, 'right');
+    const user = `<strong>${data}</strong> joined the chat`;
+    append(user, 'right');
+    // append(`${data} joined the chat`, 'right'); // without bold 
 });
 
+// if the form get submitted send server the message    
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const message = messageInp.value;
-    append(`You: ${message}`, 'right');
+    const user = `<strong>You: </strong> ${message}`;
+    append(user, 'right');
+    // append(`You: ${message}`, 'right');
     socket.emit('sendMessage', message);
     messageInp.value = '';
 });
 
+// if server send message that receive accept here 
 socket.on('receive', data => {
-    append(` ${data.name}: ${data.message}`, 'left');
+    const user = `<strong>${data.name}: </strong> ${data.message}`;
+    append(user, 'left');
+    // append(`${data.name}: ${data.message}`, 'left');
 });
 
+// if user leave to the chat then appen to the container
 socket.on('left', name => {
-    append(`${name} left the chat`, 'left');
+    const user = `<strong>${name}</strong> left the chat`;
+    append(user, 'right');
+    // append(`${name} left the chat`, 'right');
 });
 
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -8,16 +8,20 @@ const io = require('socket.io')(8000, {
 const users = {};
 
 io.on('connection', socket => {
+
+    // when join the new users all brodcast other users know by message 
     socket.on('new-user-joined', name => {
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
         // io.emit('user-joined', name); // if we want to display the user also joined 
     });
 
+    // if someone send the message get others all brodcast know 
     socket.on('sendMessage', message => {
         socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
     });
 
+    // when someone leave the chat others know by message 
     socket.on('disconnect', message => {
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
